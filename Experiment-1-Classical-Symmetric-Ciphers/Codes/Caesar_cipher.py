f079b756cd25b24f0f8c6644b73841e638a2769e
2024A7R025 <2024A7R025@mietjammu.in>
2026-09-09 05:18:24 +0530

    Implement Caesar cipher encryption function

--- Experiment-1-Classical-Symmetric-Ciphers/Codes/Caesar_cipher.py
+++ Experiment-1-Classical-Symmetric-Ciphers/Codes/Caesar_cipher.py
@@ -1,1 +1,18 @@
+def caesar_encrypt(message: str, key: int) -> str:
+
+    output = []
+    for letter in message:
+        if letter.isalpha():
+            start = ord('A') if letter.isupper() else ord('a')
+            position = (ord(letter) - start + key) % 26
+            output.append(chr(position + start))
+        else:
+            output.append(letter)
+
+    return "".join(output)
+
+message = input()
+key = int(input())
+
+print(caesar_encrypt(message, key))
 
